c9a32d86117f9abc193fa9e6294e52d1c5ab559e
Mobina Poulaei <m.poulaei@gmail.com>
2026-02-23 18:06:57 +0330

    Update data.py

--- data/data.py
+++ data/data.py
@@ -1,7 +1,7 @@
 import os
 import kagglehub
 
-new_path = "/dss/dssfs04/lwp-dss-0002/pn36fu/pn36fu-dss-0000/reza/Aghayari/CTGEN/data"
+new_path = "./data"
 os.environ["KAGGLEHUB_CACHE"] = new_path
 
 path_1 = kagglehub.dataset_download("paultimothymooney/kermany2018")
