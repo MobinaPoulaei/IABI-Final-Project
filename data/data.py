a552d7104d2854500a551cd4e91dea272fb554a9
Mobina Poulaei <m.poulaei@gmmail.com>
2026-02-23 21:57:08 +0100

    update data

--- data/data.py
+++ data/data.py
@@ -4,8 +4,8 @@ import kagglehub
 new_path = "./data"
 os.environ["KAGGLEHUB_CACHE"] = new_path
 
-path_1 = kagglehub.dataset_download("paultimothymooney/kermany2018")
-print(f"Dataset downloaded to: {path_1}")
+oct_dataset = kagglehub.dataset_download("paultimothymooney/kermany2018")
+print(f"Dataset downloaded to: {oct_dataset}")
 
-path_2 = kagglehub.dataset_download("crawford/qureai-headct")
-print(f"Dataset downloaded to: {path_2}")
+cq500_dataset = kagglehub.dataset_download("crawford/qureai-headct")
+print(f"Dataset downloaded to: {cq500_dataset}")
